f77ff3116e2c779b2874cec6984b845795ef73ab
leonwgc <giantfish@126.com>
2023-01-19 17:51:35 +0800

    feat: add global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'react-uni-comps';
+import { ThemeProvider, createGlobalStyle } from 'react-uni-comps';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(245, 247, 250);
+    font-size: 14px;
+    margin:0;
+    font-family: -apple-system, BlinkMacSystemFont, 'PingFang SC', 'Helvetica Neue', STHeiti,
+    'Microsoft Yahei', Tahoma, Simsun, sans-serif;
+    line-height: 1.5715;
+  }
+  *{
+    box-sizing: border-box;;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <ThemeProvider color={'#005cff'}>
         <Component {...pageProps} />
       </ThemeProvider>
